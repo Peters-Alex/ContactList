b9e680c7b6ce8adbb4b1039f005980b6a1b5249d
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2024-01-06 13:55:57 -0600

    intinal commit

---
+++ src/Component/ContactList.jsx
@@ -0,0 +1,27 @@
+
+
+
+export default function ContactList() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="3"> Contact List </th>
+                </tr>
+                <tr>
+                    <td> Name </td>
+                    <td> Email </td>
+                    <td> Phone </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr> 
+                    {
+                        //Map over data here
+                    }
+                </tr>
+
+            </tbody>
+        </table>
+    )
+}
